2e40b34aa98a940d988be57cf5da457fb64e292b
Ryan Long <rlong@redhat.com>
2021-03-14 13:18:09 -0400

    remove cli (#9)
    * remove cli
    
    * clean up
    
    * move files
    
    * use yarn;
    
    * update
    
    * yarn

--- README.md
+++ README.md
@@ -3,28 +3,8 @@
 Everything you need to make drinks from your CLI
 
 ## run locally
-* `npm install`
-* `npm link`
-* `npm run build`
-* `barcart make manhattan`
-
-## CLI commands
-
-### Make
-* List instructions based on a drink name: `barcart make manhattan`
-* List instructions based on a drink name (with amount): `barcart make manhattan -a 2` (this is two drinks)
-* List instructions for random drinks: `barcart make -r`
-
-### Find
-* List drinks based on glassware: `barcart find -g martini`
-* List drinks based on liquor: `barcart find -l rum`
-* List drinks based on ingredients: `barcart find -i vodka orange-juice`
-
-#### Notes:
-Liquors are normalized, this means that:
-
-* "whisky" and "whiskey" are considerd the same thing when searching
-* "white rum", "black rum", etc. are considered the same thing when searching
+* `yarn install`
+* `yarn build`
 
 ## Usage
 
@@ -32,7 +12,7 @@ All functions should return an array of objects declared in [drinkInterfaces](ht
 
 ### Make
 ```js
-import { makeByName, makeByRandom } from 'barcart/lib/make';
+import { makeByName, makeByRandom } from 'barcart/dist';
 
 const foo = makeByName('manhattan');
 const bar = makeByRandom();
@@ -40,7 +20,7 @@ const bar = makeByRandom();
 
 ### Find
 ```js
-import { findByGlass, findByLiquor, findByIngredients } from 'barcart/lib/find';
+import { findByGlass, findByLiquor, findByIngredients } from 'barcart/dist';
 
 const foo = findByGlass('martini');
 const bar = findByLiquor('rum');
--- src/make/index.ts
+++ src/make/index.ts
@@ -1,22 +1,16 @@
-#!/usr/bin/env node
-
 import { data } from '../cocktails';
-import logMakeOutput from './output';
-import { getRandomInt } from '../utils/utils';
-import { log, logBody } from '../utils/logUtils';
+import { getRandomInt } from '../utils';
 import stringSimilarity from 'string-similarity';
 import { DrinkInterface } from '../interfaces/drinkInterface';
 
-export const makeByName = (input:string, amount:number):DrinkInterface[] => {
+export const makeByName = (input: string):DrinkInterface[] => {
 
   const drinkInput = input.toLowerCase();
   const recipe = data.find(x => x.name.toLowerCase() === drinkInput);
 
   if(recipe) {
-    logMakeOutput(recipe, amount);
     return(Array(recipe));
   } else {
-    const errorString = "I don't know that one";
     const similarDrinkReceipes:DrinkInterface[] = [];
     
     // find similar strings, if above .6, log it
@@ -25,25 +19,12 @@ export const makeByName = (input:string, amount:number):DrinkInterface[] => {
         similarDrinkReceipes.push(x);
       }
     });
-
-    // output for similar drinks
-    if(similarDrinkReceipes.length) {
-      log(`${errorString}, but I did find some similar drinks:`);
-      similarDrinkReceipes.map(drink => logBody(drink.name))
-    } else {
-      log(errorString);
-    }
     return(similarDrinkReceipes);
   }
 };
 
-export const makeByRandom = (amount:number):DrinkInterface[] => {
-  
+export const makeByRandom = ():DrinkInterface[] => {
   const drinkInput = getRandomInt(data.length);
-  const recipe = data[drinkInput];
-
-  log('✨ Picking a random cocktail for you! ✨');
-  logMakeOutput(recipe, amount);
-  
+  const recipe = data[drinkInput];  
   return Array(recipe);
 };
